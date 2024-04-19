73899b0fe981c27315b34527e9c6ac56ca1a96a0
Ouail Laamiri <laamiri.ouail@etu.uae.ac.ma>
2024-04-19 19:18:34 +0100

    unittest work successfully

--- tests/main.py
+++ tests/main.py
@@ -6,7 +6,15 @@ sys.path.append(parent_dir)
 import unittest
 from fastapi.testclient import TestClient
 from src.main import app  # Assuming your FastAPI app is defined in main.py
+from dotenv import load_dotenv
+from pathlib import Path
 
+# Load variables from the specified .env file
+load_dotenv(dotenv_path=str(Path("./tests/.env")))
+
+# Access the variables
+sender_email = os.getenv("EMAIL_UNIT_TEST")
+sender_password = os.getenv("PASSWORD_UNIT_TEST")
 class TestEmailEndpoint(unittest.TestCase):
     def setUp(self):
         self.client = TestClient(app)
@@ -55,21 +63,6 @@ class TestEmailEndpoint(unittest.TestCase):
         response = self.client.post("/send/", files=files, data=form_data)
         self.assertEqual(response.status_code, 422)  # Expecting OK status code for a valid request
 
-    def test_valid_request(self):
-        # Sending valid files and form data
-        files = {
-            'emails': ('test_emails.txt', b'file content'),
-            'messages': ('test_messages.html', b'file content'),
-            'resume': ('test_resume.pdf', b'file content'),
-        }
-        form_data = {
-            'sender_email': 'example@example.com',
-            'sender_password': 'xxxx xxxx xxxx xxxx',
-            'email_subject': 'Test Subject',
-        }
-        response = self.client.post("/send/", files=files, data=form_data)
-        self.assertEqual(response.status_code, 200)  # Expecting OK status code for a valid request
-
     def test_email_connection(self):
         # Sending valid files and form data
         files = {
@@ -84,6 +77,23 @@ class TestEmailEndpoint(unittest.TestCase):
         }
         response = self.client.post("/send/", files=files, data=form_data)
         self.assertEqual(response.status_code, 601)  # Expecting OK status code for a valid request
+        
+    def test_valid_request(self):
+        # Sending valid files and form data
+        files = {
+            'emails': ('test_emails.txt', b'file content'),
+            'messages': ('test_messages.html', b'file content'),
+            'resume': ('test_resume.pdf', b'file content'),
+        }
+        form_data = {
+            'sender_email': sender_email,
+            'sender_password': sender_password,
+            'email_subject': 'Test Subject',
+        }
+        response = self.client.post("/send/", files=files, data=form_data)
+        self.assertEqual(response.status_code, 200)  # Expecting OK status code for a valid request
+
+    
 
 
 
